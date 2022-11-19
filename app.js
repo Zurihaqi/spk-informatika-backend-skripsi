a7878ba96cef84b1bf57658b91de5fd7a627f2c8
Zurihaqi <zurihaqi@gmail.com>
2022-11-19 15:50:43 +0700

    created db

---
+++ app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+// const logger = require("morgan");
+// const open = require("open");
+// const cors = require("cors");
+
+// call all module route here
+// const router = require("./server/routes/app.routes");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(logger("combined"));
+// app.use(cors());
+
+module.exports = app;
