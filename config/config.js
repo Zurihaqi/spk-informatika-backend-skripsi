434da2ab6daf1dff316b88dabfbb73265e53a239
Zul Fahri Baihaqi <72984265+Zurihaqi@users.noreply.github.com>
2023-03-06 12:37:56 +0700

    sequelize attempt 5

--- config/config.js
+++ config/config.js
@@ -3,12 +3,16 @@ require("pg");
 
 module.exports = {
   development: {
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DB_NAME,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    protocol: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     host: process.env.PG_HOST,
